e3b6f568090d192922443b38c4dc133dd8c21841
sakib <s.sarder232002@gmail.com>
2023-05-05 03:56:57 +0600

    final commit

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const categories = require('./Data/categories.json');
-const cors = require('cors');
- 
+const categories = require("./Data/categories.json");
+const cors = require("cors");
+const news = require("./Data/news.json");
+
 app.use(cors());
 
-app.get('/', (req, res) => {
- res.send("Dragon is Running")   
-})
+app.get("/", (req, res) => {
+  res.send("Dragon is Running");
+});
 
-app.get('/categories', (req, res) => {
-    res.send(categories);
-})
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id == id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
-    console.log(`Daragon api is running on port: ${port}`);
-})
+  console.log(`Daragon api is running on port: ${port}`);
+});
